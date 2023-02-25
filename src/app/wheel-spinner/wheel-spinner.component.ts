cf527af3e1d2d5202ba81b58f5f382190e7b346f
Mark Manthey <madlyinanger37@gmail.com>
2023-02-25 17:08:03 -0600

    Fix issue with clear names and code cleanup

--- src/app/wheel-spinner/wheel-spinner.component.ts
+++ src/app/wheel-spinner/wheel-spinner.component.ts
@@ -11,31 +11,17 @@ export class WheelSpinnerComponent {
 	@Input() sliceItems: string[] = [];
 	@ViewChild('svgContainer', { static: false }) svgContainer!: ElementRef;
 	
-	slices: any = [
-		{ color: 'Blue' },
-		{ color: 'Green' },
-		{ color: 'Red' },
-		{ color: 'Yellow' },
-		{ color: 'Purple' },
-	];
-
-	nameSlices: any = [];
-
 	cumulativePercent: number = 0;
-
-	spinnerWheelSvg = `<path d="M 1 0 A 1 1 0 0 1 6.123233995736766e-17 1 L 0 0" fill="Blue"></path>
-  <path d="M 6.123233995736766e-17 1 A 1 1 0 0 1 -1 1.2246467991473532e-16 L 0 0" fill="Green"></path>
-  <path d="M -1 1.2246467991473532e-16 A 1 1 0 0 1 -1.8369701987210297e-16 -1 L 0 0" fill="Red"></path>
-  <path d="M -1.8369701987210297e-16 -1 A 1 1 0 0 1 1 -2.4492935982947064e-16 L 0 0" fill="Yellow"></path>`
 
 	getCoordinatesForPercent(percent: number) {
 		const x = Math.cos(2 * Math.PI * percent);
 		const y = Math.sin(2 * Math.PI * percent);
-
+		
 		return [x, y];
 	}
 
 	getSlicePath(percent: number, fill?: string): string {
+
 		const [startX, startY] = this.getCoordinatesForPercent(this.cumulativePercent);
 
 		this.cumulativePercent += percent;
@@ -58,9 +44,10 @@ export class WheelSpinnerComponent {
 	drawWheel() {
 		
 		var wheelInnerHtml: string = '';
-		
+		this.cumulativePercent = 0;
+
 		if (this.sliceItems.length > 0) {
-				
+			
 			const percent = 1 / this.sliceItems.length;
 
 			this.sliceItems.forEach(() => {
@@ -69,7 +56,7 @@ export class WheelSpinnerComponent {
 
 			});		
 		} else {
-			wheelInnerHtml = this.getSlicePath(100, 'Black');
+			wheelInnerHtml = this.getSlicePath(1, 'DFEEFC');
 		}
 
 		this.renderer.setProperty(this.svgContainer.nativeElement, 'innerHTML', wheelInnerHtml);
