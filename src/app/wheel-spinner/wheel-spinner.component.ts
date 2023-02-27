1b2b190d61db1bfb27a0e5769849709bb030558d
Mark Manthey <madlyinanger37@gmail.com>
2023-02-26 18:38:24 -0600

    Adding item names to the wheel

--- src/app/wheel-spinner/wheel-spinner.component.ts
+++ src/app/wheel-spinner/wheel-spinner.component.ts
@@ -5,13 +5,15 @@ import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@ang
 	templateUrl: './wheel-spinner.component.html',
 	styleUrls: ['./wheel-spinner.component.css']
 })
-
 export class WheelSpinnerComponent {
 
 	@Input() sliceItems: string[] = [];
 	@ViewChild('svgContainer', { static: false }) svgContainer!: ElementRef;
 	
-	cumulativePercent: number = 0;
+	private cumulativePercent: number = 0;
+	private slicesGroupInnerHtml: string = '';
+	private defsPathInnerHtml: string = '';
+	private textPathGroupInnerHtml: string = '';
 
 	getCoordinatesForPercent(percent: number) {
 		const x = Math.cos(2 * Math.PI * percent);
@@ -20,45 +22,96 @@ export class WheelSpinnerComponent {
 		return [x, y];
 	}
 
-	getSlicePath(percent: number, fill?: string): string {
+	drawSlice(slicePercent: number, centerSlicePercent: number, sliceCount: number, fill?: string) {
 
+		// starting coordinates
 		const [startX, startY] = this.getCoordinatesForPercent(this.cumulativePercent);
 
-		this.cumulativePercent += percent;
+		this.cumulativePercent += centerSlicePercent;
 
+		// center line coordinates
+		const [centerX, centerY] = this.getCoordinatesForPercent(this.cumulativePercent);
+		
+		this.cumulativePercent += centerSlicePercent;
+
+		// ending coordinates
 		const [endX, endY] = this.getCoordinatesForPercent(this.cumulativePercent);
 
-		const largeArcFlag = percent > .5 ? 1 : 0;
+		const largeArcFlag = slicePercent > .5 ? 1 : 0;
 
-		const pathData = [
+		const slicePath = [
 			`M ${startX} ${startY}`,
 			`A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY}`,
 			`L 0 0`,
 		].join(' ');
 
+		// if (slicePercent === .25) {
+		// 	console.log(`Slice Percent: ${slicePercent}`);
+		// 	console.log(`Center Slice Percent: ${centerSlicePercent}`);
+		// 	console.log(`Starting: ${[startX, startY]}`);
+		// 	console.log(`Center: ${[centerX, centerY]}`);
+		// 	console.log(`Ending: ${[endX, endY]}`);
+		// }
+
+		const defTextPath = [
+			`M ${centerX} ${centerY}`,
+			`L 0 0`,
+		].join(' ');
+
 		var randomColor = Math.floor(Math.random()*16777215).toString(16);
-		var path = `<path d="${pathData}" fill="#${fill ?? randomColor}"></path>`;
-		return path;
+		this.slicesGroupInnerHtml += `<path d="${slicePath}" fill="#${fill ?? randomColor}"></path>`;
+
+		this.defsPathInnerHtml += `
+				<path id="textPath-${sliceCount}" d="${defTextPath}"></path>				
+		`;
+
+		this.textPathGroupInnerHtml += `
+			<text font-size=".125">
+				<textPath href="#textPath-${sliceCount}">
+					<tspan fill="white" alignment-baseline="middle">Bob</tspan>
+				</textPath>
+			</text>
+		`;
 	}
 
 	drawWheel() {
 		
 		var wheelInnerHtml: string = '';
+		this.slicesGroupInnerHtml = '';
+		this.textPathGroupInnerHtml = '';
+		this.defsPathInnerHtml = '';
+
 		this.cumulativePercent = 0;
+		var sliceCount: number = 0;
 
 		if (this.sliceItems.length > 0) {
 			
-			const percent = 1 / this.sliceItems.length;
-
+			const slicePercent = 1 / this.sliceItems.length;
+			const centerSlicePercent = 1 / (this.sliceItems.length * 2);
+			
 			this.sliceItems.forEach(() => {
-
-				wheelInnerHtml += this.getSlicePath(percent);
-
+				sliceCount++;
+				this.drawSlice(slicePercent, centerSlicePercent, sliceCount);
+				
 			});		
 		} else {
-			wheelInnerHtml = this.getSlicePath(1, 'DFEEFC');
+			sliceCount++;
+			this.drawSlice(1, sliceCount, .5, 'DFEEFC');
 		}
 
+		wheelInnerHtml += '<g>';
+		wheelInnerHtml += this.slicesGroupInnerHtml;
+		wheelInnerHtml += '</g>';
+
+		wheelInnerHtml += '<g>';
+		wheelInnerHtml += '<defs>';
+		wheelInnerHtml += this.defsPathInnerHtml;
+		wheelInnerHtml += '</defs>';
+
+		wheelInnerHtml += this.textPathGroupInnerHtml;
+		wheelInnerHtml += '</g>';
+
+		
 		this.renderer.setProperty(this.svgContainer.nativeElement, 'innerHTML', wheelInnerHtml);
 	}
 
